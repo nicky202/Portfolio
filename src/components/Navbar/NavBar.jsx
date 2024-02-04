6520809039500d1124d7b7ccea5a6f3c444c814f
nicky202 <rabesoanicky@gmail.com>
2024-02-04 20:23:13 +0300

    responsive navbar

--- src/components/Navbar/NavBar.jsx
+++ src/components/Navbar/NavBar.jsx
@@ -1,13 +1,24 @@
 import { Link } from "react-router-dom";
 import "./NavBar.scss";
+import { FaBarsStaggered } from "react-icons/fa6";
 
 import cv from "../../assets/CV - Nicky Rabesoa.pdf";
 
 const NavBar = () => {
+  const handleOpen = () => {
+    let x = document.querySelector(".topnav");
+    x.className === "topnav"
+      ? (x.className += " responsive")
+      : (x.className = "topnav");
+  };
+
   return (
     <nav className="flex">
       <h2>R. Nicky</h2>
-      <ul>
+      <button onClick={handleOpen}>
+        <FaBarsStaggered size={20} className="bar" />
+      </button>
+      <ul className="topnav">
         <li>
           <Link to="/">Home</Link>
         </li>
